619f3182050208660aa55dd3b30b5e8f5480220f
Guilherme de Freitas Laranja <guilherme@MacBook-de-Guilherme-de-Freitas-Laranja.local>
2013-05-01 10:08:08 -0300

    setup

---
+++ fgengine/src/main/java/com/ape/fgengine/App.java
@@ -0,0 +1,13 @@
+package com.ape.fgengine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
